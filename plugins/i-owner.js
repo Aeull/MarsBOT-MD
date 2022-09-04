8d2d16004b1dd98118c7728c8cac7ffd9107fd31
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-04 09:03:43 +0800

    Update i-owner.js

--- plugins/i-owner.js
+++ plugins/i-owner.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6283832492541', 'RHns', m)
+  this.sendContact(m.chat, '6285822347348', 'Arull', m)
 }
 handler.help = ['owner']
 handler.tags = ['info']
